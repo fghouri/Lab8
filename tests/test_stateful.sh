7693420ceab17e35e9e9c06c480007386e9ae109
Casey-h-vols <chiggi18@vols.utk.edu>
2026-05-03 12:06:05 -0400

    Create test_stateful.sh

---
+++ tests/test_stateful.sh
@@ -0,0 +1,6 @@
+for i in {1..50}; do echo "A User_$i -60 aa:bb:cc:dd:ee:$i"; done > input.txt
+# 20 Finds (all within the first 40)
+for i in {1..20}; do echo "F aa:bb:cc:dd:ee:$i"; done >> input.txt
+echo "P" >> input.txt
+echo "Q" >> input.txt
+./router_tester stateful 40 -100 < input.txt
