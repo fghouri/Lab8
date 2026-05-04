4c5a5dd0fb818454ed7a7fb567725dde60b7ce92
Casey-h-vols <chiggi18@vols.utk.edu>
2026-05-04 11:10:36 -0400

    Update router.hpp

--- include/router.hpp
+++ include/router.hpp
@@ -1,19 +1,24 @@
 // Router Header File to define methods and variables for the router hash table
+#ifndef ROUTER_HPP
+#define ROUTER_HPP
 
 /* ** Includes ** */
 #include <iostream>
 #include <unordered_map>
-
+#include <string>
+#include "client.hpp"
 class Router {
     public:
-        string Set_Up(const bool stateful, int max_clients = 10000, int min_rssi = 0);
-        string Add(const string &name, const int &rssi, const string &mac_address);
-        Client* Find(const string &mac_address);
-        void printAll();
-    
-    protected: 
-        unordered_map<string, Client> client_table;
+        std::string Set_Up(const bool stateful, int max_clients = 10000, int min_rssi = 0);
+        std::string Add(const std::string &name, const int &rssi, const std::string &mac_address);
+        std::string Find(const std::string &mac_address);
+        void printAll() const;
+
+    protected:
+        std::unordered_map<std::string, Client> client_table;
         bool stateful;
         int max_clients;
         int min_rssi;
 };
+
+#endif
