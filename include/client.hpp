b0bc625dbe951523707647ef91f6e82b54f52e40
Casey-h-vols <chiggi18@vols.utk.edu>
2026-05-04 11:10:02 -0400

    Update client.hpp

--- include/client.hpp
+++ include/client.hpp
@@ -1,6 +1,9 @@
 /* ** Client Header ** */
 // Purpose: Structure implementation for client.hpp
 
+#ifndef CLIENT_HPP
+#define CLIENT_HPP
+
 /* ** Includes ** */
 #include <iostream>
 
@@ -10,13 +13,15 @@ class Client {
     public:
         // Constructor
         Client(const string &name, const int &rssi, const string &mac_address);
-        string getName();
-        string getMAC();
-        int getRSSI(); 
-        void Print_Data();
+        string getName() const;
+        string getMAC() const;
+        int getRSSI() const;
+        void Print_Data() const;
+        string String_Data();
 
     private:
         string name;
         string mac_address;
         int rssi;
 };
+#endif
