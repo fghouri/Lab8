528b6e6441f179a6868c2894b78acd40f7f0a1b4
Casey-h-vols <chiggi18@vols.utk.edu>
2026-05-05 21:08:03 -0400

    Update router.cpp

--- src/router.cpp
+++ src/router.cpp
@@ -8,7 +8,7 @@
 #include <string>
 #include <unordered_map>
 #include <sstream>
-
+#include <iomanip>
 // Purpose: Sets router initial state based on ./ initialization
 string Router::Set_Up(const bool stateful, int max_clients, int min_rssi)
 {
