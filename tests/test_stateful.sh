718e0dd2469ef1edbb34ceff2b5e311b3e4f59c6
Casey-h-vols <chiggi18@vols.utk.edu>
2026-05-05 21:11:00 -0400

    Update test_stateful.sh

--- tests/test_stateful.sh
+++ tests/test_stateful.sh
@@ -3,4 +3,4 @@ for i in {1..50}; do echo "A User_$i -60 aa:bb:cc:dd:ee:$i"; done > input.txt
 for i in {1..20}; do echo "F aa:bb:cc:dd:ee:$i"; done >> input.txt
 echo "P" >> input.txt
 echo "Q" >> input.txt
-./router_tester stateful 40 -100 < input.txt
+./bin/router_tester stateful 40 -100 < input.txt
